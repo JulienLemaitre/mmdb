8da28eae324594efae1e2320da2b328253d37289
JulienLemaitre <julienlemaitre80+github@pm.me>
2024-05-11 14:33:06 +0200

    Improved db persistence error return

--- app/api/feedForm/route.ts
+++ app/api/feedForm/route.ts
@@ -10,17 +10,16 @@ import getDecodedTokenFromReq from "@/utils/getDecodedTokenFromReq";
 
 export async function POST(request: NextRequest) {
   if (!isReqAuthorized(request)) {
-    return new Response(JSON.stringify({ error: "Unauthorized" }), {
-      status: 401,
-    });
+    return NextResponse.json({ error: `Unauthorized` }, { status: 401 });
   }
 
   const decodedToken = await getDecodedTokenFromReq(request);
   const creatorId = decodedToken?.id;
   if (!creatorId) {
-    return new Response(JSON.stringify({ error: "Unauthorized creator" }), {
-      status: 401,
-    });
+    return NextResponse.json(
+      { error: `No authorized creator found` },
+      { status: 401 },
+    );
   }
 
   const state = (await request.json()) as FeedFormState;
@@ -57,31 +56,33 @@ export async function POST(request: NextRequest) {
     getMMSourceAndRelatedEntitiesDBInputFromState(state, creatorId);
 
   // Execute all operations in a single transaction
-  const results = await db.$transaction(async (tx) => {
-    const mMSource = await tx.mMSource.create({
-      data: mMSourceInput,
-      include: {
-        references: true,
-        pieceVersions: {
-          include: {
-            pieceVersion: {
-              include: {
-                movements: {
-                  include: {
-                    sections: {
-                      include: {
-                        tempoIndication: true,
-                        metronomeMarks: {
-                          include: {
-                            mMSource: {
-                              include: {
-                                contributions: {
-                                  include: {
-                                    person: true,
-                                    organization: true,
+  return await db
+    .$transaction(async (tx) => {
+      const mMSource = await tx.mMSource.create({
+        data: mMSourceInput,
+        include: {
+          references: true,
+          pieceVersions: {
+            include: {
+              pieceVersion: {
+                include: {
+                  movements: {
+                    include: {
+                      sections: {
+                        include: {
+                          tempoIndication: true,
+                          metronomeMarks: {
+                            include: {
+                              mMSource: {
+                                include: {
+                                  contributions: {
+                                    include: {
+                                      person: true,
+                                      organization: true,
+                                    },
                                   },
+                                  references: true,
                                 },
-                                references: true,
                               },
                             },
                           },
@@ -94,38 +95,44 @@ export async function POST(request: NextRequest) {
             },
           },
         },
-      },
+      });
+
+      console.log(`[] mMSource :`, JSON.stringify(mMSource));
+      const mMSourceId = mMSource.id;
+      console.log(`[] mMSourceId :`, mMSourceId);
+
+      console.log(
+        `[] mMSource pieceVersions id :`,
+        mMSource.pieceVersions.map((pv) => pv.pieceVersionId),
+      );
+
+      const metronomeMarksInput: Prisma.MetronomeMarkCreateManyInput[] =
+        state.metronomeMarks
+          .filter((metronomeMark) => !metronomeMark.noMM)
+          .map((metronomeMark) =>
+            getMetronomeMarkDBInputFromState(metronomeMark, mMSourceId),
+          );
+
+      console.log(
+        `[] metronomeMarksInput sectionId`,
+        metronomeMarksInput.map((m) => m.sectionId),
+      );
+
+      const metronomeMarks = await tx.metronomeMark.createMany({
+        data: metronomeMarksInput,
+      });
+
+      console.log(`[] metronomeMarks :`, metronomeMarks);
+
+      return { mMSource, metronomeMarks };
+    })
+    .then((results) => {
+      return NextResponse.json(results);
+    })
+    .catch((error) => {
+      return NextResponse.json(
+        { error: `Error during DB persistence: ${error.message}` },
+        { status: 500 },
+      );
     });
-
-    console.log(`[] mMSource :`, JSON.stringify(mMSource));
-    const mMSourceId = mMSource.id;
-    console.log(`[] mMSourceId :`, mMSourceId);
-
-    console.log(
-      `[] mMSource pieceVersions id :`,
-      mMSource.pieceVersions.map((pv) => pv.pieceVersionId),
-    );
-
-    const metronomeMarksInput: Prisma.MetronomeMarkCreateManyInput[] =
-      state.metronomeMarks
-        .filter((metronomeMark) => !metronomeMark.noMM)
-        .map((metronomeMark) =>
-          getMetronomeMarkDBInputFromState(metronomeMark, mMSourceId),
-        );
-
-    console.log(
-      `[] metronomeMarksInput sectionId`,
-      metronomeMarksInput.map((m) => m.sectionId),
-    );
-
-    const metronomeMarks = await tx.metronomeMark.createMany({
-      data: metronomeMarksInput,
-    });
-
-    console.log(`[] metronomeMarks :`, metronomeMarks);
-
-    return { mMSource, metronomeMarks };
-  });
-
-  return NextResponse.json(results);
 }
