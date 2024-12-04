32c71f705fa934200666b221d84a9b976df1580b
JulienLemaitre <julienlemaitre80+github@pm.me>
2024-12-04 21:45:53 +0100

    Allow Enter action on Button and Textarea only

--- utils/preventEnterKeySubmission.ts
+++ utils/preventEnterKeySubmission.ts
@@ -1,10 +1,25 @@
 import React from "react";
 
+/**
+ * Prevents form submission when the Enter key is pressed, except for textarea and button elements.
+ * This function is useful for handling keyboard events in forms to avoid unintended submissions.
+ *
+ * @param e - The React keyboard event object from the form element.
+ * @returns void
+ *
+ * @example
+ * <form onKeyDown={preventEnterKeySubmission}>
+ *   // Form content
+ * </form>
+ */
 export default function preventEnterKeySubmission(
   e: React.KeyboardEvent<HTMLFormElement>,
 ) {
   const target = e.target as HTMLFormElement;
-  if (e.key === "Enter" && !["TEXTAREA"].includes(target.tagName)) {
+  const isTextarea = target.tagName === "TEXTAREA";
+  const isButton = target.tagName === "BUTTON";
+
+  if (e.key === "Enter" && !(isTextarea || isButton)) {
     console.log(`PREVENT Enter key submission!`);
     e.preventDefault();
   }
