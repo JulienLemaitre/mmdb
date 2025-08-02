b57ab0a388d737d8eede5e5e800c80a72de6f14f
JulienLemaitre <julienlemaitre80+github@pm.me>
2025-08-02 16:02:29 +0200

    Accent button for Add new entity buttons

--- components/ReactSelect/getNoOptionsMessage.tsx
+++ components/ReactSelect/getNoOptionsMessage.tsx
@@ -1,3 +1,5 @@
+import PlusIcon from "@/components/svg/PlusIcon";
+
 type GetNoOptionsMessageProps = {
   router?: any;
   entityName: string;
@@ -17,13 +19,14 @@ export default function getNoOptionsMessage({
         <div className="ml-4 mb-2">{`No ${entityName} found`}</div>
         <button
           type="button"
-          className="btn btn-primary"
+          className="btn btn-accent"
           onClick={() => {
             console.log(`Create a new ${entityName}`);
             if (typeof onClick === "function") onClick();
             if (router && createUrl) router.push(createUrl);
           }}
         >
+          <PlusIcon className="w-5 h-5" />
           {`Create a new ${entityName}`}
         </button>
       </div>
