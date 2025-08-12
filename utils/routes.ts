ad58c003957db44c8481343c42007a9468c7d3f3
JulienLemaitre <julienlemaitre80+github@pm.me>
2025-08-12 15:11:20 +0200

    fix: not camelCased names and unused routes and folders

--- utils/routes.ts
+++ utils/routes.ts
@@ -13,23 +13,3 @@ export const URL_API_FEEDFORM_SUBMIT = "/api/feedForm";
 export const URL_HOME = "/";
 export const URL_EXPLORE = "/explore";
 export const URL_FEED = "/feed";
-export const URL_EDITION = "/edition";
-export const URL_SELECT_COMPOSER = "/edition/composer";
-export const URL_CREATE_COMPOSER = "/edition/composer/create";
-export const URL_UPDATE_COMPOSER = "/edition/composer/update";
-export const URL_SELECT_PIECE = "/edition/piece";
-export const URL_CREATE_PIECE = "/edition/piece/create";
-export const URL_UPDATE_PIECE = "/edition/piece/update";
-export const URL_SELECT_PIECE_VERSION = "/edition/piece-version";
-export const URL_CREATE_PIECE_VERSION = "/edition/piece-version/create";
-export const URL_UPDATE_PIECE_VERSION = "/edition/piece-version/update";
-export const URL_CREATE_SOURCE_DESCRIPTION =
-  "/edition/source-description/create";
-export const URL_UPDATE_SOURCE_DESCRIPTION =
-  "/edition/source-description/update";
-export const URL_CREATE_SOURCE_CONTRIBUTIONS =
-  "/edition/source-contributions/create";
-export const URL_UPDATE_SOURCE_CONTRIBUTIONS =
-  "/edition/source-contributions/update";
-export const URL_CREATE_METRONOME_MARKS = "/edition/metronome-marks/create";
-export const URL_UPDATE_METRONOME_MARKS = "/edition/metronome-marks/update";
