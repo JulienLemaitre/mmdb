45eb492b468b43eb91d91d09f2899dca7d5f46c7
JulienLemaitre <julienlemaitre80+github@pm.me>
2025-11-05 23:38:59 +0100

    fix: jest config to transform uuid module

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,32 @@
-const nextJest = require('next/jest')
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
-    // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-    dir: './',
-})
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-    testEnvironment: 'jest-environment-jsdom',
-}
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testEnvironment: "jest-environment-jsdom",
+};
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+// // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+// module.exports = createJestConfig(customJestConfig);
+
+// Source: https://github.com/vercel/next.js/discussions/31152#discussioncomment-1697047
+// createJestConfig returns an async function that returns a jest config -
+// so instead of doing this:
+// module.exports = createJestConfig(customJestConfig)
+
+// Take the returned async function...
+const asyncConfig = createJestConfig(customJestConfig);
+
+// and wrap it...
+module.exports = async () => {
+  const config = await asyncConfig();
+  config.transformIgnorePatterns = [
+    "/node_modules/(?!uuid)/", // transform uuid package to use it in tests
+  ];
+  return config;
+};
